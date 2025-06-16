ffbd456ace6871fa8de06abccff50db33d7afe7b
Bharat <bharatprasad@Bharats-MacBook-Air.local>
2025-06-16 11:35:22 +0530

    Controller class

--- src/main/java/com/example/InventoryManagement/controller/Controller.java
+++ src/main/java/com/example/InventoryManagement/controller/Controller.java
@@ -4,10 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ControllerIM {
+public class Controller {
 
     @GetMapping("/home")
-    public String sayHello(){
-        return "Welcome to Inventory Project !!";
+    public String welCome(){
+        return "Welcome to Inventory management Project !!";
     }
 }
